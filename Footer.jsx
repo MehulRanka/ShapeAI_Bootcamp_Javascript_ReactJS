07165c2f8337f8886ca223604e2154f703315de5
MehulRanka <84791438+MehulRanka@users.noreply.github.com>
2021-06-05 23:11:34 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  var currYear = new Date().getFullYear;
+
+  return (
+    <footer>
+      <p> Copyright @ 2021 </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div classname="note">
+      <h1> Javascript and React JS Bootcamp</h1>
+      <p>
+        {" "}
+        This was an amazing bootcamp. I enjoyed a lot in the bootcamp. mostly I
+        had a good experience and great knowledge from it that would surely help
+        me in the future.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Note;
